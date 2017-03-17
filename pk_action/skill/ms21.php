4264b2a57dc9ec8f6a53d1f02cc7f0f31b1ff34a
caiweihan <caiweihan@local.host>
2017-03-17 10:19:58 +0800

    1

--- pk_action/skill/ms21.php
+++ pk_action/skill/ms21.php
@@ -5,7 +5,7 @@
 	class sm_21_0 extends SkillBase{
 		public $isAtk = true;
 		function action($user,$self,$enemy){
-			$this->decHp($user,$enemy,$user->atk*1.8);
+			$this->decHp($user,$enemy,$user->atk*2);
 		}
 	}
 	
