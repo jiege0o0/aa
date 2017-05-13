26203baab538727a806c79d836a91a6c34c6ed8e
caiweihan <caiweihan@local.host>
2017-05-13 14:57:16 +0800

    1

--- buy_rmb.php
+++ buy_rmb.php
@@ -2,10 +2,10 @@
 	$id = $msg->id;
 	$arr = array(
 		'1'=>array('cost'=>12),
-		'101'=>array('cost'=>6,'diamond'=>60),
-		'102'=>array('cost'=>30,'diamond'=>305),
-		'103'=>array('cost'=>150,'diamond'=>1530),
-		'104'=>array('cost'=>600,'diamond'=>6200)
+		'101'=>array('cost'=>6,'diamond'=>100),
+		'102'=>array('cost'=>30,'diamond'=>520),
+		'103'=>array('cost'=>150,'diamond'=>2650),
+		'104'=>array('cost'=>600,'diamond'=>10650)
 	);
 	
 	
---
+++ diamond_draw.php
@@ -0,0 +1,55 @@
+<?php 
+	require_once($filePath."cache/monster.php");
+	do{
+		$time = time();
+		if(!$userData->active->draw_time)
+			$userData->active->draw_time = 0;
+		if($time - $userData->active->draw_time < 4*3600)
+		{
+			$returnData->fail = 1;
+			$returnData->sync_active = new stdClass();
+			$returnData->sync_active->draw_time = $userData->active->draw_time;
+			$returnData->sync_active->draw_num = $userData->active->draw_num;
+			break;
+		}
+		
+		if(!isSameDate($userData->active->draw_time))
+		{
+			$userData->active->draw_num = 0;
+		}
+		
+		if($userData->active->draw_num == 3)
+		{
+			$returnData->fail = 2;
+			$returnData->sync_active = new stdClass();
+			$returnData->sync_active->draw_time = $userData->active->draw_time;
+			$returnData->sync_active->draw_num = $userData->active->draw_num;
+			break;
+		}
+			
+		$cost = 10;
+		while(lcg_value()>0.5)
+			$cost += 5;
+		$arr = array();
+		foreach($monster_base as $key=>$value)
+		{
+			if($userData->level >= $value['level'] && $value['cost'] <= $cost)
+			{	
+				array_push($arr,$value);
+			}
+		}
+		$value = $arr[rand(0,count($arr) - 1)];
+		$returnData->cardid = $value['id'];
+		$userData->addDiamond($value['cost']);
+		$userData->active->draw_time = $time;
+		$userData->active->draw_num ++;
+		$returnData->sync_active = new stdClass();
+		$returnData->sync_active->draw_time = $userData->active->draw_time;
+		$returnData->sync_active->draw_num = $userData->active->draw_num;
+		$userData->setChangeKey('active');
+		// $userData->write2DB();
+	}while(false);
+	
+
+
+?> 
